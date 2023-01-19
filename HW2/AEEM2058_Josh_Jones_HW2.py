3f1d9d35f1d41da19c19a849ca05501c06e1c643
jonesjoshmax <jonesjoshmax@gmail.com>
2023-01-19 01:58:22 -0500

    Updated

--- HW2/AEEM2058_Josh_Jones_HW2.py
+++ HW2/AEEM2058_Josh_Jones_HW2.py
@@ -3,6 +3,7 @@ import compFunc as cf
 import time
 import random as r
 import matplotlib.pyplot as plt
+import pandas as pd
 
 
 def ab_refresh(a, b):
@@ -25,8 +26,7 @@ def matrixInverse(a):
 
 
 def part3():
-    runtime = np.empty((3, 1))
-    validation = np.copy(runtime)
+    rt_Val = np.empty((3, 2))
     # Given arrays from problem 11
     a = np.array([
         [10, -2, -1, 2, 3, 1, -4, 7],
@@ -39,35 +39,44 @@ def part3():
         [-1, 3, 4, 1, 3, -4, 7, 6]])
     b = np.array([[0], [12], [-5], [3], [-25], [-26], [9], [-7]])
     b = b * -1
+    a = a.astype('float64')
+    b = b.astype('float64')
 
     # Cramer Calculation
-    a_ref, b_ref = ab_refresh(a, b)
     t0 = time.perf_counter()
-    x = cf.cramer(a_ref, b_ref)
-    a_ref, b_ref = ab_refresh(a, b)
-    v = np.sum(np.array(np.dot(a_ref, x) - b_ref))
+    x = cf.cramer(a.copy(), b.copy())
     t = time.perf_counter() - t0
-    runtime[0, 0] = t
-    validation[0, 0] = v
+    v = np.sum(np.array(np.dot(a.copy(), x) - b.copy()))
+    rt_Val[0, 0] = t * 1000000
+    rt_Val[0, 1] = v
 
     # Gauss Calculation
-    a_ref, b_ref = ab_refresh(a, b)
     t0 = time.perf_counter()
-    x = cf.gaussPivot(a_ref, b_ref)
-    a_ref, b_ref = ab_refresh(a, b)
-    v = np.sum(np.array(np.dot(a_ref, x) - b_ref))
+    x = cf.gaussPivot(a.copy(), b.copy())
     t = time.perf_counter() - t0
-    runtime[1, 0] = t
-    validation[1, 0] = v
+    v = np.sum(np.array(np.dot(a.copy(), x) - b.copy()))
+    rt_Val[1, 0] = t * 1000000
+    rt_Val[1, 1] = v
 
     # LU Calculation
+    t0 = time.perf_counter()
+    a0, seq = cf.LUpivot(a.copy())
+    x = cf.LUsolve(a0.copy(), b.copy(), seq.copy())
+    t = time.perf_counter() - t0
+    v = np.sum(np.array(np.dot(a.copy(), x) - b.copy()))
+    rt_Val[2, 0] = t * 1000000
+    rt_Val[2, 1] = v
 
-    return runtime, validation
+    data = {'Runtime (microseconds)': [rt_Val[0, 0], rt_Val[1, 0], rt_Val[2, 0]],
+            'Accuracy': [rt_Val[0, 1], rt_Val[1, 1], rt_Val[2, 1]]
+            }
+    df = pd.DataFrame(data, index=['Cramer', 'Gauss', 'LU'])
+    print(df)
 
 
 def part4():
     cycles = [2, 4, 6, 8, 12]
-    runtime = np.empty((3, len(cycles)))
+    runtime = np.empty((3, len(cycles))).astype('float64')
     validation = np.copy(runtime)
     # Array creation / loop for testing
     for h in range(len(cycles)):
@@ -81,52 +90,60 @@ def part4():
         b = b.reshape((n, 1))
 
         # Cramer Calculation
-        a_ref, b_ref = ab_refresh(a, b)
         t0 = time.perf_counter()
-        x = cf.cramer(a_ref, b_ref)
-        a_ref, b_ref = ab_refresh(a, b)
-        v = np.sum(np.array(np.dot(a_ref, x) - b_ref))
+        x = cf.cramer(a.copy(), b.copy())
         t = time.perf_counter() - t0
+        v = np.sum(np.array(np.dot(a.copy(), x) - b.copy()))
         runtime[0, h] = t
         validation[0, h] = v
 
         # Gauss Calculation
-        a_ref, b_ref = ab_refresh(a, b)
         t0 = time.perf_counter()
-        x = cf.gaussPivot(a_ref, b_ref)
-        a_ref, b_ref = ab_refresh(a, b)
-        v = np.sum(np.array(np.dot(a_ref, x) - b_ref))
+        x = cf.gaussPivot(a.copy(), b.copy())
         t = time.perf_counter() - t0
+        v = np.sum(np.array(np.dot(a.copy(), x) - b.copy()))
         runtime[1, h] = t
         validation[1, h] = v
 
-        # LU Function
+        # LU Calculation
+        t0 = time.perf_counter()
+        a0, seq = cf.LUpivot(a.copy())
+        x = cf.LUsolve(a0.copy(), b.copy(), seq.copy())
+        t = time.perf_counter() - t0
+        v = np.sum(np.array(np.dot(a.copy(), x) - b.copy()))
+        runtime[2, h] = t
+        validation[2, h] = v
+    runtime = runtime / 0.000001
 
     return runtime, validation, cycles
 
 
-def prob4_visualization(runtime, validation, cycles):
-    matrix = runtime
-    name = 'Runtime'
-    for i in range(2):
-        x = np.arange(len(cycles))
-        width = .25
-        fig, ax = plt.subplots()
-        cr = ax.bar(x - width, matrix[0, :], width, label='Cramer', color='tomato')
-        ga = ax.bar(x, matrix[1, :], width, label='Gauss', color='bisque')
-        lu = ax.bar(x + width, matrix[2, :], width, label='LU', color='lightskyblue')
-        ax.set_xlabel('N x N Matrix')
-        ax.set_ylabel(name)
-        ax.set_xticks(x, cycles)
-        ax.legend()
-        ax.set_title('{} of N x N Matrices'.format(name), weight='bold')
-        ax.bar_label(cr, padding=2)
-        ax.bar_label(ga, padding=2)
-        ax.bar_label(lu, padding=2)
-        fig.tight_layout()
-        plt.show()
-        matrix = validation
-        name = 'Residual Value'
+def part4_visualization(runtime, validation, cycles):
+    x = np.arange(len(cycles))
+    width = .25
+    fig, ax = plt.subplots()
+    cr = ax.bar(x - width, runtime[0, :], width, label='Cramer', color='tomato')
+    ga = ax.bar(x, runtime[1, :], width, label='Gauss', color='bisque')
+    lu = ax.bar(x + width, runtime[2, :], width, label='LU', color='lightskyblue')
+    ax.set_xlabel('N x N Matrix')
+    ax.set_ylabel('Runtime (microseconds)')
+    ax.set_xticks(x, cycles)
+    ax.legend()
+    ax.set_title('Runtime of N x N Matrices', weight='bold')
+    ax.bar_label(cr, padding=2)
+    ax.bar_label(ga, padding=2)
+    ax.bar_label(lu, padding=2)
+    fig.tight_layout()
+    plt.show()
+    fig2, ax2 = plt.subplots()
+    ax2.plot(cycles, validation[0, :], label='Cramer', color='tomato', linewidth=3)
+    ax2.plot(cycles, validation[1, :], label='Gauss', color='bisque', linewidth=3)
+    ax2.plot(cycles, validation[2, :], label='LU', color='lightskyblue', linewidth=3)
+    ax2.set_xlabel('N x N Matrix')
+    ax2.set_ylabel('Accuracy')
+    ax2.set_title('Accuracy of N x N Matrix', weight='bold')
+    ax2.legend()
+    plt.show()
 
 
 def part5():
@@ -137,25 +154,11 @@ def part5():
         [3, 3, -1, 2]
     ])
     prob8_inverse = matrixInverse(prob8_array)
-    print(np.round(np.dot(prob8_inverse, prob8_array)))
+    print('Part 5\nOriginal Matrix:\n{}\nInverted Matrix:\n{}'.format(prob8_array, prob8_inverse))
+    print('Dot Product of Original & Inverted:\n{}'.format(np.round(np.dot(prob8_inverse, prob8_array))))
 
 
-def lu_testing():
-    a = np.array([
-        [10, -2, -1, 2, 3, 1, -4, 7],
-        [5, 11, 3, 10, -3, 3, 3, -4],
-        [7, 12, 1, 5, 3, -12, 2, 3],
-        [8, 7, -2, 1, 3, 2, 2, 4],
-        [2, -15, -1, 1, 4, -1, 8, 3],
-        [4, 2, 9, 1, 12, -1, 4, 1],
-        [-1, 4, -7, -1, 1, 1, -1, -3],
-        [-1, 3, 4, 1, 3, -4, 7, 6]])
-    b = np.array([[0], [12], [-5], [3], [-25], [-26], [9], [-7]])
-    b = b * -1
-
-    a0 = cf.LUdecomp(a.copy())
-    x = cf.LUsolve_decomp(a0.copy(), b.copy())
-
-    return x
-
-
+part3()
+rt, valid, cyc = part4()
+part4_visualization(rt, valid, cyc)
+part5()
